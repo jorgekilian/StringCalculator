5980c5b3f5672dedb985d7ab0ef2132c00ce8b51
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-02-21 17:24:42 +0000

    green: return_sum_when_string_is_four_any_numbers_separated_only_by_carriage_return

--- StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
+++ StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
@@ -6,7 +6,7 @@ namespace StringCalculatorSpecs {
     public abstract class StringCalculator {
         public static int Add(string numbers) {
             if (numbers == string.Empty) return 0;
-            if (!numbers.Contains(",")) return int.Parse(numbers);
+            if (!numbers.Contains(",") && !numbers.Contains("\n")) return int.Parse(numbers);
             if (numbers.Contains("\n")) numbers = numbers.Replace("\n", ",");
             return numbers.Split(",").Sum(int.Parse);
         }
