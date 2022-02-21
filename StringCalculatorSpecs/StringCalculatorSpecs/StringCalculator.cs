69987db32cfbdba6820fe9f67bcfd20c5a7dfa36
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-02-21 17:07:24 +0000

    refactor: return_sum_when_string_is_two_any_numbers

--- StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
+++ StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
@@ -1,9 +1,11 @@
+using System.Linq;
+
 namespace StringCalculatorSpecs {
     public abstract class StringCalculator {
         public static int Add(string numbers) {
             if (numbers == string.Empty) return 0;
             if (!numbers.Contains(",")) return int.Parse(numbers);
-            return 3;
+            return numbers.Split(",").Sum(int.Parse);
         }
     }
 }
