0be3f99922d47afb40af99e2a88180cf8a54ebe8
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-02-21 16:43:53 +0000

    green: return_2_when_string_is_2

--- StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
+++ StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
@@ -1,7 +1,12 @@
 namespace StringCalculatorSpecs {
     public abstract class StringCalculator {
         public static int Add(string numbers) {
-            if (numbers != string.Empty) return 1;
+            if (numbers != string.Empty)
+                if (numbers == "1")
+                    return 1;
+                else {
+                    return 2;
+                }
             return 0;
         }
     }
