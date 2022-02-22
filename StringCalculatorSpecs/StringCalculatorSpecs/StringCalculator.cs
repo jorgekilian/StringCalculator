b50d8f8c46cadce474e237e2d2ca97888ea0278e
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-02-22 12:13:29 +0000

    refactor

--- StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
+++ StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
@@ -3,14 +3,15 @@ using System.Linq;
 namespace StringCalculatorSpecs {
     public abstract class StringCalculator {
         public static int Add(string numbers) {
+            var separator = ",";
             if (numbers == string.Empty) return 0;
             if (!numbers.Contains(",") && !numbers.Contains("\n")) return int.Parse(numbers);
             if (numbers.Contains("//")) {
-                var separator = numbers.Substring(2, 1);
+                separator = ";";
                 numbers = numbers.Substring(4);
-                numbers = numbers.Replace(separator, ",");
             }
-            if (numbers.Contains("\n")) numbers = numbers.Replace("\n", ",");
+            if (numbers.Contains("\n")) separator = "\n";
+            numbers = numbers.Replace(separator, ",");
             return numbers.Split(",").Sum(int.Parse);
         }
     }
