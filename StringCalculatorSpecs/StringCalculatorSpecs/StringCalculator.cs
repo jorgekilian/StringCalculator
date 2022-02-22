8e61e87f786963e0db0e2d5bca9bc30bea8716ab
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-02-22 12:03:49 +0000

    green: return_sum_when_string_is_separated_by_a_semicolon_separator_defi

--- StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
+++ StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
@@ -1,12 +1,15 @@
 using System.Linq;
-using System.Threading.Channels;
-using Microsoft.VisualBasic;
 
 namespace StringCalculatorSpecs {
     public abstract class StringCalculator {
         public static int Add(string numbers) {
             if (numbers == string.Empty) return 0;
             if (!numbers.Contains(",") && !numbers.Contains("\n")) return int.Parse(numbers);
+            if (numbers.Contains("//")) {
+                var separator = numbers.Substring(2, 1);
+                numbers = numbers.Substring(4);
+                numbers = numbers.Replace(separator, ",");
+            }
             if (numbers.Contains("\n")) numbers = numbers.Replace("\n", ",");
             return numbers.Split(",").Sum(int.Parse);
         }
