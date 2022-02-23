d5c02a3d2bd53ec2bca2603d54b383c6a1bb3e45
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-02-23 17:47:33 +0000

    green: return_sum_when_string_is_separated_by_a_multiple_separators

--- StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
+++ StringCalculatorSpecs/StringCalculatorSpecs/StringCalculator.cs
@@ -1,26 +1,38 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StringCalculatorSpecs {
     public abstract class StringCalculator {
         public static int Add(string numbers) {
-            var separator = ",";
+            var separator = new List<string>();
             if (numbers == string.Empty) return 0;
             if (HasOnlyOneNumber(numbers)) return int.Parse(numbers);
             if (HasSpecificSeparator(numbers)) {
-                separator = numbers.Substring(2, 1);
                 var endDelimiter = numbers.IndexOf("\n", StringComparison.Ordinal);
-                if (separator == "[") {
-                    var pos = numbers.IndexOf("]", 2, StringComparison.Ordinal);
-                    separator = numbers.Substring(3, pos - 3);
+                var separatorString = numbers.Substring(2, endDelimiter - 2);
+                var beginDelimiter = separatorString.IndexOf("[", StringComparison.Ordinal);
+                if (beginDelimiter > -1) {
+                    while (separatorString.IndexOf("]", StringComparison.Ordinal) > -1) {
+                        separator.Add(separatorString.Substring(1, separatorString.IndexOf("]", StringComparison.Ordinal) - 1));
+                        separatorString = separatorString.Substring(separatorString.IndexOf("]", StringComparison.Ordinal) + 1);
+                    }
+                }
+                else {
+                    separator.Add(numbers.Substring(2, 1));
                 }
                 numbers = numbers.Substring(endDelimiter + 1);
             }
-            if (numbers.Contains("\n")) separator = "\n";
-            var intNumbers = numbers.Replace(separator, ",")
-                                                .Split(",")
-                                                .Select(int.Parse)
-                                                .Where(x => x <= 1000);
+            if (numbers.Contains("\n")) separator.Add("\n");
+
+            if (separator.Count > 0) {
+                foreach (var sep in separator) {
+                    numbers = numbers.Replace(sep, ",");
+                }
+            }
+            var intNumbers = numbers.Split(",")
+                .Select(int.Parse)
+                .Where(x => x <= 1000);
             var negativeNumbers = intNumbers.Where(i => i < 0).Select(x => Convert.ToString(x)); ;
             if (negativeNumbers.Any()) throw new Exception($"Negatives not allowed ({string.Join(",", negativeNumbers) })");
             return intNumbers.Sum();
